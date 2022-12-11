c6ddd0830156ebb1a6c1f50ce2be4449c8ad1465
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 <=>
2022-12-11 21:54:48 +0000

    First commit

--- extractdata.py
+++ extractdata.py
@@ -10,38 +10,38 @@ authSource = 'admin'
 client = MongoClient(host=full_dns_name, username=username, password=password, authSource=authSource)
 
 #afficher la liste des bases de données disponibles
-print( "Liste de base de donne:", client.list_database_names())
+print( "\n*********(b)afficher la liste des bases de données disponibles:", client.list_database_names())
 
 #afficher la liste des collections disponibles dans cette base de données
 arxiv = client["arxiv"]
-print("Liste de collection de la bdd arvix", arxiv.list_collection_names())
+print("\n*********(c)afficher la liste des collections disponibles dans cette base de données", arxiv.list_collection_names())
 
 #afficher un des documents de cette collection
+print("\n*********(d)afficher un des documents de cette collection")
 pprint(arxiv.papers.find_one())
 
 #afficher le nombre de documents dans cette collection
-print("Nombre de documents in collection papers:", arxiv.papers.count_documents({}))
+print("\n*********(e)afficher le nombre de documents dans cette collection:", arxiv.papers.count_documents({}))
 
-#afficher le titre de 10 articles
-print("\n*********Titres \n:")
+
+print("\n*********(a)afficher le titre de 10 articles \n:")
 for t in list(arxiv.papers.find({},{"title":1, "_id":0}).limit(10)):
     print(t)
-print("\n*********Autheurs \n")
-#afficher les 10 premiers auteurs par ordre alphabétique
+print("\n*********(b)afficher les 10 premiers auteurs par ordre alphabétique \n")
 for a in list( arxiv.papers.find({},{"authors":1, "_id":0}).sort("authors", 1).limit(10)):
     print(a)
 
 # afficher le nombre d'articles qui n'ont pas été publiés par "Damien Chablat"
-print("Le nombre d'articles pas publie par Damien est:", arxiv.papers.count_documents({'submitter':{'$ne':'Damien Chablat'}}))
+print("\n*********(c)afficher le nombre d'articles qui n'ont pas été publiés par Damien Chablat:", arxiv.papers.count_documents({'submitter':{'$ne':'Damien Chablat'}}))
 
 # afficher le nombre de papiers téléversés en 2014. On pourra s'appuyer sur une expréssion régulière.
-print("Le nombre des papiers televersé en 2004 est:", arxiv.papers.count_documents({'update_date':{'$regex':'^2014'}}))
+print("\n*********(d)afficher le nombre de papiers téléversés en 2014:", arxiv.papers.count_documents({'update_date':{'$regex':'^2014'}}))
 
 #afficher le titre d'un article qui contient l'expression Machine Learning. On pourra utiliser une expression régulière.
-print("Titrearticle qui contient Machine Learning:", arxiv.papers.find_one({"title" : {'$regex':'Machine Learning'}},{"title":1, "_id":0}))
+print("\n*********(e)afficher le titre d'un article qui contient l'expression Machine Learning:", arxiv.papers.find_one({"title" : {'$regex':'Machine Learning'}},{"title":1, "_id":0}))
 
 #afficher le nombre de publications par submitter pour les 10 personnes les plus prolifiques. 
 # On pourra utiliser un pipeline d'aggrégation avec les mots-clefs $group, $sort, $limit.
-print("\n nombre de publications par submitter pour les 10 personnes les plus prolifiques")
+print("\n*********(f)afficher le nombre de publications par submitter pour les 10 personnes les plus prolifiques.")
 for v in list(arxiv.papers.aggregate([{"$group" :{"_id":"$submitter", "np":{"$sum" : 1 }}}, {"$sort" : {"np":-1}}, {"$limit" : 10}])):
     print(v)
